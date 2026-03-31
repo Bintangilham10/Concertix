6a8d6c779f4ce2e0baa7213e6482d7be285aa016
Bintangilham10 <bintangilham.dev@gmail.com>
2026-03-31 23:38:24 +0700

    blockchain block model and export in models package

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,5 +2,6 @@ from app.models.user import User
 from app.models.concert import Concert
 from app.models.ticket import Ticket
 from app.models.transaction import Transaction
+from app.models.blockchain import Block
 
-__all__ = ["User", "Concert", "Ticket", "Transaction"]
+__all__ = ["User", "Concert", "Ticket", "Transaction", "Block"]
